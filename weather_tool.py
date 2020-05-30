40c9352e0069f221824b410b0842abe00e94e0b0
Poornima J <poornijaidip@gmail.com>
2020-05-30 17:33:44 +0530

    modified temperature in Celsius and Fahreinheit

--- weather_tool.py
+++ weather_tool.py
@@ -3,10 +3,12 @@ import json
 from pprint import pprint
 
 city = input("Enter name of city")
-url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid=***Enter your API Key here***".format(city)
+url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid=***Enter your API Key here***&units=metric".format(city)
 res = requests.get(url)
 data = res.json()
 #pprint(data) This is to print the dictionary in a pretty way to understand it better
-temp_k = data['main']['temp']
+temp_c = data['main']['temp']
+temp_f = temp_c*1.8+32
 
-print("Temperature of {} is {} kelvin".format(city,temp_k))
+print("Temperature of {} is {} Celsius".format(city,temp_c))
+print("Temperature of {} is {} Fahreinheit".format(city,temp_f))
