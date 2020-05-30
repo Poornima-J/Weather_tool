a30c5f6db2b69ea4fc22c14355d504571d3a4e94
Poornima J <poornijaidip@gmail.com>
2020-05-30 18:03:24 +0530

    modified to display an emoji about the present weather condition

--- weather_tool.py
+++ weather_tool.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import emoji
 from pprint import pprint
 
 city = input("Enter name of city")
@@ -9,6 +10,20 @@ data = res.json()
 #pprint(data) This is to print the dictionary in a pretty way to understand it better
 temp_c = data['main']['temp']
 temp_f = temp_c*1.8+32
+weather = data['weather'][0]['main']
+if weather == 'clear':
+    emoj = ':Sun:'
+elif weather == 'Thunderstorm':
+    emoj = ':Cloud With Rain:'
+elif weather == 'Drizzle':
+    emoj = ':Sun Behind Rain Cloud:'
+elif weather == 'Rain':
+    emoj = ':Umbrella With Rain Drops:'
+elif weather == 'Snow':
+    emoj = ':Cloud With Snow:'
+elif weather == 'Clouds':
+    emoj = ':Cloud:'                 
 
 print("Temperature of {} is {} Celsius".format(city,temp_c))
 print("Temperature of {} is {} Fahreinheit".format(city,temp_f))
+print("Weather is:{}".format(emoji.emojize(emoj)))
